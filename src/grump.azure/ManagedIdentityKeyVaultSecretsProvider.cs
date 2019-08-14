8f2912793e875aea730662fca4e0292ec924b390
Adrian Padilla <adrian@adrianpadilla.net>
2019-08-14 13:16:54 -0700

    Managed Identity Keyvaul secrets provider

--- src/grump.azure/ManagedIdentityKeyVaultSecretsProvider.cs
+++ src/grump.azure/ManagedIdentityKeyVaultSecretsProvider.cs
@@ -1,22 +1,80 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Grump.Abstractions;
+using Microsoft.Azure.KeyVault;
 using Microsoft.Extensions.Configuration;
 
 namespace Grump.Azure
 {
     public class ManagedIdentityKeyVaultSecretsProvider : ISecretsProvider
     {
-        private readonly IConfigurationProvider _configurationProvider;
+        private readonly IConfiguration _configuration;
 
-        public ManagedIdentityKeyVaultSecretsProvider(IConfigurationProvider configurationProvider)
+        private IKeyVaultClient _keyVaultClient;
+
+
+        public ManagedIdentityKeyVaultSecretsProvider(IConfiguration configuration)
         {
-            _configurationProvider = configurationProvider ?? throw new ArgumentNullException("configurationProvider");
+            _configuration = configuration ?? throw new ArgumentNullException("configuration");
         }
+
+        public IKeyVaultClient KeyVaultClient
+        {
+            get
+            {
+                if (_keyVaultClient == null)
+                {
+                    _keyVaultClient = new ManagedIdentityKeyVaultClient();
+                }
+
+                return _keyVaultClient;
+            }
+        }
+
+        public string KeyVaultUrl
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_keyVaultUrl))
+                {
+                    _keyVaultUrl = GetKeyVaultUrlFromConfiguration(_configuration);
+                }
+
+                return _keyVaultUrl;
+            }
+        }
+
+        private string _keyVaultUrl;
+
+
+        /// <summary>
+        /// Creates an new instance of the class ManagedIdentityKeyVaultSecretsProvider (Intended for testing).
+        /// </summary>
+        /// <param name="configurationProvider"></param>
+        /// <param name="keyVaultClient"></param>
+        /// <remarks>
+        ///     The intention of this constructor is testing, by providing a mock IKeyVaultClient instance.
+        /// </remarks>
+        internal ManagedIdentityKeyVaultSecretsProvider(IConfiguration configuration, IKeyVaultClient keyVaultClient): this(configuration)
+        {
+            _keyVaultClient = keyVaultClient;
+        }
+
         public async Task<string> GetSecretAsync(string secretName)
         {
-            throw new System.NotImplementedException();
+            var secretUrl = $"{KeyVaultUrl}/secrets/{secretName}";
+
+           var secret = await KeyVaultClient.GetSecretAsync(secretUrl).ConfigureAwait(false);
+
+            return secret.Value;
         }
-        
+
+        internal static string GetKeyVaultUrlFromConfiguration(IConfiguration configuration)
+        {
+            //TODO: Validation etc
+            return configuration["KeyVault:Url"];
+        }
+
+
     }
 }
