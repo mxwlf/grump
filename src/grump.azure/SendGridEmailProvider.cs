e5c16543279f8483765aa94cfbbe6d8c582f7800
Adrian <adrian@adrianpadilla.net>
2020-01-14 22:35:02 -0800

    Sendgrid started

---
+++ src/grump.azure/SendGridEmailProvider.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Grump.Abstractions;
+
+namespace Grump.Azure
+{
+    public class SendGridEmailProvider
+    {
+
+        public async Task SendEmail(EmailMessage message)
+        {
+            
+        }
+            
+    }
+}
